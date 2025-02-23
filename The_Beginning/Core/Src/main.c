0479fb27a30948ba7c4bd7b53705770db9e2eaeb
Francisco Becerra <32798992+FKBecerra@users.noreply.github.com>
2025-02-22 23:43:58 -0600

    We're ready for bare-metal programming!!
    I made this basic template for future projects using microcontrollers based on the ARM architecture from the vendor ST Microelectronic and the CMSIS direct register programming, without the HAL Library.
    Specifically, this template is made for the hardware contained in the STM32 NUCLEO-F429ZI development board.

---
+++ The_Beginning/Core/Src/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ *  Created on: Feb 22, 2025
+ *      Author: Francisco Becerra
+ */
+#include "main.h"
+
+int main(void){
+
+	while(1){
+
+	}
+
+	return 0;
+}
---
+++ The_Beginning/Debug/Core/Src/system_stm32f4xx.d
@@ -0,0 +1,17 @@
+Core/Src/system_stm32f4xx.o: ../Core/Src/system_stm32f4xx.c \
+ C:/Users/Engineering/GitHub/Embedded_Systems/The_Beginning/Core/Inc/stm32f4xx.h \
+ C:/Users/Engineering/GitHub/Embedded_Systems/The_Beginning/Core/Inc/stm32f429xx.h \
+ C:/Users/Engineering/GitHub/Embedded_Systems/The_Beginning/CMSIS/Include/core_cm4.h \
+ C:/Users/Engineering/GitHub/Embedded_Systems/The_Beginning/CMSIS/Include/cmsis_version.h \
+ C:/Users/Engineering/GitHub/Embedded_Systems/The_Beginning/CMSIS/Include/cmsis_compiler.h \
+ C:/Users/Engineering/GitHub/Embedded_Systems/The_Beginning/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/Engineering/GitHub/Embedded_Systems/The_Beginning/CMSIS/Include/mpu_armv7.h \
+ C:/Users/Engineering/GitHub/Embedded_Systems/The_Beginning/Core/Inc/system_stm32f4xx.h
+C:/Users/Engineering/GitHub/Embedded_Systems/The_Beginning/Core/Inc/stm32f4xx.h:
+C:/Users/Engineering/GitHub/Embedded_Systems/The_Beginning/Core/Inc/stm32f429xx.h:
+C:/Users/Engineering/GitHub/Embedded_Systems/The_Beginning/CMSIS/Include/core_cm4.h:
+C:/Users/Engineering/GitHub/Embedded_Systems/The_Beginning/CMSIS/Include/cmsis_version.h:
+C:/Users/Engineering/GitHub/Embedded_Systems/The_Beginning/CMSIS/Include/cmsis_compiler.h:
+C:/Users/Engineering/GitHub/Embedded_Systems/The_Beginning/CMSIS/Include/cmsis_gcc.h:
+C:/Users/Engineering/GitHub/Embedded_Systems/The_Beginning/CMSIS/Include/mpu_armv7.h:
+C:/Users/Engineering/GitHub/Embedded_Systems/The_Beginning/Core/Inc/system_stm32f4xx.h:
